64dd1ba4caf08590545e8e18ce828ef32bc6d2fe
siktirmirza <alicowman79@gmail.com>
2016-12-27 16:56:24 +0330

    Update microsys.lua

--- microsys.lua
+++ microsys.lua
@@ -45,12 +45,12 @@ function tdcli_update_callback(data)
 			local lowrank_msg = 'شما دسترسی کافی برای این کار را ندارید'
     -- If the message is text message
 		if msg.content_.ID == "MessageText" then
-			if input:match('^مقام من$') and not is_sudo(msg) and not is_mod(msg) then
+			if input:match('^مقام من$') and not is_sudo(msg) then
 				tdcli.sendText(msg.chat_id_, 0, 0, 1, nil, '_شما یک کاربر ساده هستید_', 1, 'md')
 			elseif input:match('^مقام من$') and is_sudo(msg) then
 				tdcli.sendText(msg.chat_id_, 0, 0, 1, nil, '_تو بابای منی :/_', 1, 'md')
-			elseif input:match('^مقام من$') and is_mod(msg) then
-				tdcli.sendText(msg.chat_id_, 0, 0, 1, nil, '_شما مدیر گروه هستید_', 'md')
+			--elseif input:match('^مقام من$') and is_mod(msg) then
+			--	tdcli.sendText(msg.chat_id_, 0, 0, 1, nil, '_شما مدیر گروه هستید_', 'md')
 			--elseif input:match('^مقام من$') and is_owner(msg) then
 			--	tdcli.sendText(msg.chat_id_, 0, 0, 1, nil, '_شما رهبر گروه هستید_', 'md')
 			end
