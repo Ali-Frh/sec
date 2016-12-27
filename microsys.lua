528f8618d852ac10830c7175567041ef0c39dbd7
siktirmirza <alicowman79@gmail.com>
2016-12-27 16:43:00 +0330

    Update microsys.lua

--- microsys.lua
+++ microsys.lua
@@ -24,7 +24,7 @@ sudo_users = {
 function is_mod(msg)
   local var = false
  -- — Check users id in config
-for v,user in redis:hget('mod'..msg.chat_id,msg.sender_user_id_) do
+for v,user in redis:hget('mod'..msg.chat_id_,msg.sender_user_id_) do
    if user == msg.sender_user_id_ then
      var = true
   end
